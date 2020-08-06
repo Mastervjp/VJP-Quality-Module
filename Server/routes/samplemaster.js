d933ccda307dce382e63d5d8f4512d38691001fd
Gowtham1698 <gowtham16.ironman@gmail.com>
2020-08-06 15:30:10 +0530

    Instrument image changed to name in cfir and fir,Sampling order changed ,process delete reflecting in sampling.

--- Server/routes/samplemaster.js
+++ Server/routes/samplemaster.js
@@ -31,7 +31,7 @@ function sendSuccess1(res, result, msg) {
 
 router.get('/:drgId', (req, res) => {
     return new Promise((resolve, reject) => {
-        Operation.findAll({ where: { drgId: req.params.drgId }, include: [Process]}).then(function (result) {
+        Operation.findAll({ where: { drgId: req.params.drgId, deleteStatus: false }, include: [Process]}).then(function (result) {
             sendSuccess(res, result);
         }).catch(function (err) {
             sendError(res, err);
