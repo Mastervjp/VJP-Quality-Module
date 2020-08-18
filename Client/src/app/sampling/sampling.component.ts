a951c34c8f8d8b1876b223dbf41e995842218b24
dev1Ironman <66583162+dev1Ironman@users.noreply.github.com>
2020-08-18 18:09:19 +0530

    Revert "sampling delete, loading changes"

--- Client/src/app/sampling/sampling.component.ts
+++ Client/src/app/sampling/sampling.component.ts
@@ -239,31 +239,31 @@ export class SamplingComponent implements OnInit {
      
       
     }
-    // for (let element of tempData) {
-    //   element.drgId = JSON.parse(localStorage.getItem('drgObject')).id;
-    //   if (element.id) {
-    //     let status = { "status": null }
-    //     this._qualityservice.approvalsample(element.drgId, status).subscribe((res: any) => {
-    //     });
+    for (let element of tempData) {
+      element.drgId = JSON.parse(localStorage.getItem('drgObject')).id;
+      if (element.id) {
+        let status = { "status": null }
+        this._qualityservice.approvalsample(element.drgId, status).subscribe((res: any) => {
+        });
 
-    //     let masterStatus = { "masterStatus": null }
-    //     this._qualityservice.approvalSampleMaster(element.drgId, masterStatus).subscribe((res: any) => {
-    //     });
-
-
-    //     let operatorStatus = { "operatorStatus": null }
-    //     this._qualityservice.updatesamplestatus(element.drgId, operatorStatus).subscribe((res: any) => {
-    //     });
+        let masterStatus = { "masterStatus": null }
+        this._qualityservice.approvalSampleMaster(element.drgId, masterStatus).subscribe((res: any) => {
+        });
 
 
+        let operatorStatus = { "operatorStatus": null }
+        this._qualityservice.updatesamplestatus(element.drgId, operatorStatus).subscribe((res: any) => {
+        });
 
 
-    //     let id = localStorage.getItem('DrgCode')
-    //     let techApproval = { "techApproval": null }
-    //     this._drawingservice.updatestatus(element.drgId, techApproval).subscribe((res: any) => {
-    //     });
-    //   }
-    // }
+
+
+        let id = localStorage.getItem('DrgCode')
+        let techApproval = { "techApproval": null }
+        this._drawingservice.updatestatus(element.drgId, techApproval).subscribe((res: any) => {
+        });
+      }
+    }
     
      this.snackBar.open("Process updated successfully", "", {
 
